9dea898758608b1d2ef0b98479efe20f097635a1
Tim <jtcii2@gmail.com>
2024-07-08 15:14:25 -0400

    initial state with a couple components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { PropsWithChildren, useCallback, useEffect, useState } from 'react';
 import './App.css';
 
+const Heading = ({ title }: { title: string }) => <h2>{title}</h2>;
+
+const Box: React.FunctionComponent<PropsWithChildren> = ({ children }) => (
+  <div
+    style={{
+      padding: "1rem",
+      fontWeight: "bold",
+    }}
+  >
+    {children}
+  </div>
+);
+
+const List: React.FunctionComponent<{
+  items: string[];
+  onClick?: (item: string) => void
+}> = ({ items, onClick }) => (
+  <ul>
+    {items.map((item, index) => (
+      <li key={index} onClick={() => onClick?.(item)}>{item}</li>
+    ))}
+  </ul>
+);
+
+interface Payload {
+  text: string;
+}
+
 function App() {
+  const onListClick = useCallback((item: string) => {
+    alert(item);
+  }, []);
+
+  const [payload, setPayload] = useState<Payload | null>(null);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setPayload(data)
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading title="Introduction" />
+      <Box>hello</Box>
+      <List items={["one", "two", "three"]} onClick={onListClick}></List>
+      <Box>{JSON.stringify(payload)}</Box>
     </div>
   );
 }
